99e425d7102aade0bca76880e88c66c98b8f46e9
SudeeptoBose <sudeeptobose23@gmail.com>
2024-06-17 14:00:03 +0600

    added model

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
 import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei"
 import { Canvas, useFrame} from "@react-three/fiber"
 import { easing } from 'maath'
+import { Model } from "./Model"
+
 
 function Scene(){
-
 	const rotationFactor = 0.2
-
+	
 	useFrame((state, delta)=>{
 		easing.dampE(state.camera.rotation, [state.pointer.y * rotationFactor, -state.pointer.x * rotationFactor, 0], 0.25, delta)
 	})
 
 	return(
 		<>
-			<mesh>
-				<boxGeometry/>
-				<meshBasicMaterial color={'orangered'}/>
-			</mesh>
-			<mesh position={[0,-0.5,0]} rotation={[-Math.PI/2, 0, 0]}>
-				<planeGeometry args={[100, 100]} />
-				<meshBasicMaterial color={'rebeccapurple'}/>
-			</mesh>
+			<Model position={[3.00,1.00,-2.00]} rotation={[0,-3.00,0]}/>
 		</>
 	)
 }
 
 function App() {
-
 	return (
 		<>
 			<Canvas>
-				{/* <OrbitControls/> */}
-
 				<Scene/>
-
 				<GizmoHelper alignment="bottom-right" margin={[80, 80]}>
 					<GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="white" />
 				</GizmoHelper>
